d9a15ebb4d5f6c5eb519b4ee284a1920ec1d3cd7
MDAHRAR72 <ahrarmd72@gmail.com>
2025-08-22 14:05:47 +0600

    working on the menu

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "@/components/menu";
+import Navbar from "@/components/navbar";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/*left*/}
+      <div className="w-1/6">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={30} height={30} />
+          <span className="hidden lg:block">Smart Class</span>
+        </Link>
+        <Menu />
+      </div>
+
+      {/*right*/}
+      <div className="w-5/6 bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between p-4">
+      {/*search bar*/}
+      <div className="hidden md:flex">
+        <Image src="/search.png" alt="" width={14} height={14} />
+        <input type="text" placeholder="Search..." />
+      </div>
+      {/*avatar and icons*/}
+      <div className="">
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image src="/message.png" alt="" width={20} height={20} />
+        </div>
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image src="/announcement.png" alt="" width={20} height={20} />
+        </div>
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image src="/message.png" alt="" width={20} height={20} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

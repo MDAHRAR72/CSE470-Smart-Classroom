f7a8a47406435cb68e388c290828b8be3d787caa
MDAHRAR72 <ahrarmd72@gmail.com>
2025-08-24 18:05:06 +0600

    Finished Single Teacher Page

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,5 +1,8 @@
+import Announcements from "@/components/Announcements";
 import BigCalendar from "@/components/BigCalendar";
+import Performance from "@/components/Performance";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const SingleTeacherPage = () => {
@@ -107,13 +110,36 @@ const SingleTeacherPage = () => {
           </div>
         </div>
         {/*Bottom*/}
-        <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
+        <div className="mt-4 bg-white rounded-xl p-4 h-[800px]">
           <h1 className="text-lg font-semibold">Teacher&apos;s Schedule</h1>
           <BigCalendar />
         </div>
       </div>
-      {/*Left*/}
-      <div className="w-full xl:w-1/3">r</div>
+      {/*Right*/}
+      <div className="w-full xl:w-1/3 flex flex-col gap-4 ">
+        <div className="bg-white p-4 rounded-xl">
+          <h1 className="text-xl font-semibold">Shortcuts</h1>
+          <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+            <Link className="p-3 rounded-xl bg-blue-100" href="/">
+              Teacher&apos;s Classes
+            </Link>
+            <Link className="p-3 rounded-xl bg-purple-100" href="/">
+              Teacher&apos;s Students
+            </Link>
+            <Link className="p-3 rounded-xl bg-blue-100" href="/">
+              Teacher&apos;s Lessons
+            </Link>
+            <Link className="p-3 rounded-xl bg-purple-100" href="/">
+              Teacher&apos;s Exams
+            </Link>
+            <Link className="p-3 rounded-xl bg-blue-100" href="/">
+              Teacher&apos;s Assignments
+            </Link>
+          </div>
+        </div>
+        <Performance />
+        <Announcements />
+      </div>
     </div>
   );
 };
